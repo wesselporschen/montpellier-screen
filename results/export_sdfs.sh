4ffa0db3ccbbc6034d44c32fb7858fe213b21f80
wesselporschen <wesselporschen@gmail.com>
2026-06-17 13:15:46 +0200

    results rg2 rg3

--- results/export_sdfs.sh
+++ results/export_sdfs.sh
@@ -6,7 +6,7 @@ for db in *.db; do
     name="${db%.db}"
     echo "Processing $name"
 
-    rt_process_vs read --input_db "${name}.db" --bookmark_name "all_actives_${name}" -sdf "${name}_sdf"
+    rt_process_vs read --input_db "${name}.db" --bookmark_name "all_montpellier_${name}" -sdf "${name}_sdf"
     rt_process_vs read --input_db "${name}.db" --bookmark_name "all_decoys_${name}" -sdf "${name}_sdf"
 
 done 
